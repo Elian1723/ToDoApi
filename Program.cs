557a63f65a6b4fd3f9401677ba45f0789a554255
Elian1723 <elianbarrios1707@outlook.com>
2025-09-10 12:09:33 -0600

    Update database in memory to database file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using ToDoApi.Mapping;
 using ToDoApi.Data;
 using ToDoApi.Repositories;
 using ToDoApi.Services;
+using ToDoApi.Models.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +22,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 // Configure SQLite in-memory database
 builder.Services.AddDbContext<ToDoDbContext>(options =>
 {
-    var connectionString = "DataSource=:memory:";
+    var connectionString = "DataSource=Data/todoapi.db";
     options.UseSqlite(connectionString);
 });
 
@@ -54,29 +55,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
